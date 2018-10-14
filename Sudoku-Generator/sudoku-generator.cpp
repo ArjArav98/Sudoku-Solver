23399908491c454efb39db2d30e4ee0223989234
arjun1098 <arjun.aravind1998@gmail.com>
2018-10-15 00:37:50 +0530

    Create Original Random Class for Sudoku-Gen
    Created an original Random class from scratch for the Sudoku-Generator file. Also added a few other classes to the sudoku-generator.cpp file. Need to definitely work on the Sudoku-Frame class there. Need to add a Sudoku-Generator class somewhere there.

--- Sudoku-Generator/sudoku-generator.cpp
+++ Sudoku-Generator/sudoku-generator.cpp
@@ -1,59 +1,90 @@
 #include<iostream>
-#include <ctime>
+#include<ctime>
 using namespace std;
 
-class SudokuGenerator{
+class Random{
 	
 	int randomCount;
 
-	public:SudokuGenerator(){
-		randomCount=17;
+	public:Random(){
+		randomCount=0;
+	}
+	
+	public:int generate(){
+		time_t now = time(0);
+		tm *curtime = localtime(&now);
+
+		int number1=(curtime->tm_sec)*(curtime->tm_sec/10);
+		int number2=(curtime->tm_min)/(curtime->tm_sec/10);
+		
+		randomCount+=7;		
+
+		return ((number1*number2)+randomCount)%9;
 	}
 
-	int randomSudoku(){
-		time_t now = time(0);
-	        tm *curtime = localtime(&now);
-	
-	        int num = (curtime->tm_year)/(curtime->tm_min) * (curtime->tm_sec + curtime->tm_min);
-	        num = num+(curtime->tm_mday*curtime->tm_mon);
-	        num = num-curtime->tm_hour;
-	
-	        int div1 = (curtime->tm_sec*13)/curtime->tm_min;
-	        int div2 = (curtime->tm_sec*4)+(curtime->tm_sec*29);
-	
-		div1+=randomCount;
-	        div1 = (div1+div2)%(curtime->tm_sec+div1);
-	        div1 = (div1+curtime->tm_sec)*curtime->tm_sec;
-	        div1 = (div1+num)+(num+div2);
-	
-	        if(curtime->tm_sec%10==1) div1=div2*9;
-	        else if(curtime->tm_sec%10==2) div1=div2*(num%9);
-	        else if(curtime->tm_sec%10==3) div1=(num*13)%(num+20)+div2;
-	        else if(curtime->tm_sec%10==4) div1=div1*17;
-	        else if(curtime->tm_sec%10==5) div1=div1+(div1*5);
-	        else if(curtime->tm_sec%10==6) div1=div1+91;
-	        else if(curtime->tm_sec%10==7) div1=(div2-19)+(num*12);
-        	else if(curtime->tm_sec%10==8) div1=(div2+div1)+(div1+num);
-	        else if(curtime->tm_sec%10==9) div1=(num+num)+23;
-
-		div1=div1&num;
-		div1=div1|num;
-
-		randomCount++;
-
-		return (div1%9);
+	public:int chooseBetween(int lower, int upper){
+		int difference=(upper+1)-lower;
+		int random=generate()%difference;
+		return (lower+random);
 	}
 
 };
 
-int main(){
+class SudokuFrame{
 	
-	SudokuGenerator s;
-	int i;
+	int sudokuFrame[9][9];
+	Random random;
 
-	for(i=0; i<10; i++){
-		cout<<s.randomSudoku();
+	public:SudokuFrame(){
+		fillFrameWithZeroes();
+		generateFrame();
+		displayFrame();
 	}
 
+	void fillFrameWithZeroes(){
+		int rowIter, colIter;
+
+		for(rowIter=0; rowIter<9; rowIter++){
+			for(colIter=0; colIter<9; colIter++){
+				sudokuFrame[rowIter][colIter]=0;
+			}
+		}
+	}
+	
+	/*void generateFrame(){
+		
+		int rowIter;
+
+		for(rowIter=0; rowIter<9; rowIter++){
+			int numOfFilledValues=random.chooseBetween(3,4);
+			int valueIter;
+
+			for(valueIter=0; valueIter<numOfFilledValues; valueIter++){
+				sudokuFrame[rowIter][random.generate()]=random.generate();		
+			}
+		}
+
+	}*/
+
+	private:void displayFrame(){
+		int rowIter, colIter;
+
+		for(rowIter=0; rowIter<9; rowIter++){
+			cout<<"+---+---+---+---+---+---+---+---+---+\n";
+			for(colIter=0; colIter<9; colIter++){
+				cout<<"| ";
+				if(sudokuFrame[rowIter][colIter]==0) cout<<"  ";
+				else cout<<sudokuFrame[rowIter][colIter]<<" ";
+			}
+			cout<<"|\n";
+		}
+
+		cout<<"+---+---+---+---+---+---+---+---+---+\n";
+
+	}
+};
+
+int main(){
+	SudokuFrame frame;
 	return 0;
 }
