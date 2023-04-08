8007ded41969af78e6fe05a710fe31d982497131
Arjun Aravind <arjun.aravind1998@gmail.com>
2023-04-08 19:13:12 +0100

    Add validator function; Add tests

--- src/sudoku_suite.h
+++ src/sudoku_suite.h
@@ -268,4 +268,20 @@ bool solve(
     return false;
 }
 
+bool is_valid_solution(
+    const Grid& grid,
+    Coord curr_coord = std::make_pair(0, 0)
+) {
+    int value = grid.get(curr_coord);
+    if (value == 0) return false;
+    else if (
+        grid.value_exists_elsewhere_in_column(curr_coord, value) ||
+        grid.value_exists_elsewhere_in_row(curr_coord, value) ||
+        grid.value_exists_elsewhere_in_3x3_grid(curr_coord, value))
+        return false;
+    if (curr_coord == std::make_pair(8, 8)) return true;
+    else
+        return is_valid_solution(grid, get_next_cell_coord(curr_coord));
+}
+
 }  // namespace sudoku
